248bf5656e5f0d7730089a6b7078a31f3a179dd7
harusame_0616 <kakino1999@gmail.com>
2021-06-02 19:21:28 +0900

    画像の修正 コルーチンを作りたかった

--- DXライブラリ/DrawPixel.cpp
+++ DXライブラリ/DrawPixel.cpp
@@ -3,11 +3,17 @@ using namespace DxLib;
 
 #define PI 3.1415926535897932384626433832795f
 
-#define SHOT 2
+#define SHOT 5
+
+int Coroutine(int time, int set);
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR IpcmdLine, int nCmdShow)
 {
-	int BallX, BallY, BallGraph;
+	int BallX, BallY, BallGraph[4];
+	int BallGraphCounter, BallMoveFlag;
+
+	int set = 0;
+
 	int SikakuX, SikakuY, SikakuMuki, SikakuGraph, Sikakureverse;
 	int SikakuDamageFlag, SikakuDamageCounter, SikakuDamageGraph;
 	int ShotX[SHOT], ShotY[SHOT], ShotFlag[SHOT], ShotGraph;
@@ -24,8 +30,6 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR IpcmdLine
 	SetGraphMode(640, 480, 16);
 	ChangeWindowMode(true);
 
-	int set = 0;
-
 	//DXライブラリ初期化処理
 	if (DxLib_Init() == -1)	return -1;
 
@@ -35,8 +39,15 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR IpcmdLine
 	SetDrawScreen(DX_SCREEN_BACK);
 
 	//ボール君のグラフィックをメモリにロード＆表示座標をセット
-	BallGraph = LoadGraph("../Graphic/パックマン1.png");
+	BallGraph[0] = LoadGraph("../Graphic/パックマン1.png");
+	BallGraph[1] = LoadGraph("../Graphic/パックマン2.png");
+	BallGraph[2] = LoadGraph("../Graphic/パックマン3.png");
+	BallGraph[3] = LoadGraph("../Graphic/パックマン4.png");
 	BallX = 320; BallY = 350; Ballrotate = PI / 2;
+	BallGraphCounter = 0;
+
+	//ボール君が動いているかどうかの変数に(動いていない)を表す0を代入
+	BallMoveFlag = 0;
 
 	//四角君のグラフィックをメモリにロード＆表示座標をセット
 	SikakuGraph = LoadGraph("../Graphic/ブリンキー.png");
@@ -73,7 +84,23 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR IpcmdLine
 	//移動ルーチン
 	while (ProcessMessage() == 0 && CheckHitKey(KEY_INPUT_ESCAPE) == 0)
 	{
-		if (set == 3)set = 0;
+		if (set == 2)set = 0;
+
+		//ボール君が動いているなら絵が切り替わるかためにカウンタを加算する
+		if (BallMoveFlag == 1)
+		{
+			BallGraphCounter++;
+
+			//もしダメージを受け始めて３０フレーム経過していたらダメージ状態から
+			//元に戻してあげる
+			if (BallGraphCounter == 3)
+			{
+				//『ダメージを受けていない』を表す０を代入
+				set++;
+				BallGraphCounter = 0;
+			}
+		}
+
 
 		//画面を初期化(真っ黒にする)
 		ClearDrawScreen();
@@ -85,27 +112,31 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR IpcmdLine
 				// ↑キーを押していたらボール君を上に移動させる
 				if (CheckHitKey(KEY_INPUT_UP) == 1)
 				{
-					set++;
 					BallY -= 3;
 					Ballrotate = PI / 2;
+					BallMoveFlag = 1;
 				}
 				// ↓キーを押していたらボール君を下に移動させる
 				else if (CheckHitKey(KEY_INPUT_DOWN) == 1)
 				{
-					set++;
 					BallY += 3;
+					BallMoveFlag = 1;
 				}
 				// ←キーを押していたらボール君を左に移動させる
 				else if (CheckHitKey(KEY_INPUT_LEFT) == 1)
 				{
-					set++;
 					BallX -= 3;
+					BallMoveFlag = 1;
 				}
 				// →キーを押していたらボール君を右に移動させる
 				else if (CheckHitKey(KEY_INPUT_RIGHT) == 1)
 				{
-					set++;
 					BallX += 3;
+					BallMoveFlag = 1;
+				}
+				else
+				{
+					BallMoveFlag = 0;
 				}
 			}
 
@@ -113,6 +144,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR IpcmdLine
 			//スペースキーを押した場合は処理を分岐
 			if (CheckHitKey(KEY_INPUT_SPACE))
 			{
+
+				set = 3;
 				//前フレームでショットボタンを押したかが保存されている変数が0だったら弾を発射
 				if (ShotBFlag == 0)
 				{
@@ -125,12 +158,12 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR IpcmdLine
 							int Bw, Bh, Sw, Sh;
 
 							//ボール君と弾の画像のサイズを得る
-							GetGraphSize(BallGraph, &Bw, &Bh);
+							GetGraphSize(BallGraph[set], &Bw, &Bh);
 							GetGraphSize(ShotGraph, &Sw, &Sh);
 
 							//弾 i の位置をセット、位置はボール君の中心にする
-							ShotX[i] = BallX - 10;
-							ShotY[i] = BallY + 10;
+							ShotX[i] = (Bw - Sw) / 2 + BallX;
+							ShotY[i] = (Bh - Sh) / 2 + BallY - 20;
 
 							//弾 iは現時点を持って存在するので、存在状態を保持する変数に１を代入する
 							ShotFlag[i] = 1;
@@ -143,12 +176,17 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR IpcmdLine
 
 				//前フレームでショットボタンを押されていたかを保存する変数に１（押されていた）を代入
 				ShotBFlag = 1;
+
+				//コルーチンを作る
+				set = Coroutine(15, 0);
 			}
 			else
 			{
 				//ショットボタンが押されていなかった場合は
 				//前フレームでショットボタンが押されていたかを保存する変数に０（おされていない）を代入
 				ShotBFlag = 0;
+
+				
 			}
 
 
@@ -164,7 +202,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR IpcmdLine
 			// ボール君が画面下端からはみ出そうになっていたら画面内の座標に戻してあげる
 			if (BallY > 480)BallY = 0;
 
-			DrawRotaGraph2(BallX, BallY, 32, 32, 1, Ballrotate, BallGraph, TRUE, TRUE);
+			DrawRotaGraph2(BallX, BallY, 32, 32, 1, Ballrotate, BallGraph[set], TRUE, TRUE);
 		}
 
 		//弾の数だけ弾を動かす処理を繰り返す
@@ -174,7 +212,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR IpcmdLine
 			if (ShotFlag[i] == 1)
 			{
 				//弾iを16ドット上に移動させる
-				ShotY[i] -= 16;
+				ShotY[i] -= 4;
 
 				//画面外に出てしまった場合は存在状態を保持している変数に0（存在しない）を代入する
 				if (ShotY[i] < -80)
@@ -192,7 +230,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR IpcmdLine
 			if (SikakuDamageFlag == 1)
 			{
 				//ダメージを受けている場合はダメージグラフィックを描画する
-				DrawRotaGraph2(SikakuX, SikakuY, 32, 32, 1, 0, SikakuDamageGraph, FALSE, Sikakureverse);
+				DrawRotaGraph2(SikakuX, SikakuY, 32, 32, 1, 0, SikakuDamageGraph, TRUE, Sikakureverse);
 				//ダメージを受けている時間を測るカウンターに1を加算する
 				SikakuDamageCounter++;
 
@@ -266,7 +304,6 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR IpcmdLine
 		ScreenFlip();
 
 
-
 		// Windows 特有の面倒な処理をＤＸライブラリにやらせる
 		// -1 が返ってきたらループを抜ける
 		if (ProcessMessage() < 0) break;
@@ -281,4 +318,19 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR IpcmdLine
 	DxLib::DxLib_End();
 
 	return 0;
+}
+
+int Coroutine(int time, int set)
+{
+	int corTime = 0;
+	for (int i = 0; i < time; i++)
+	{
+		if (i == time - 1)
+		{
+			corTime++;
+			if (corTime == 10)
+				break;
+		}
+	}
+	return set;
 }
